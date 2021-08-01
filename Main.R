077d7a14cb74e4cee9a07cc07a319d9a7edf119e
lonnibesancon <lonni.besancon@gmail.com>
2021-08-01 17:27:30 +1000

    Factoring functions

--- Main.R
+++ Main.R
@@ -4,8 +4,6 @@
 # CC-BY Lonni Besançon et al., 2021
 ################################################################
 
-##'@importFrom xml2 read_html
-##' @importFrom rvest html_table html_nodes html_text html_children
 
 #Check that "likert" is installed
 if(!require(scholar)){
@@ -56,38 +54,6 @@ resp <- get_scholar_resp("https://scholar.google.fr/citations?user=ulkW7fgAAAAJ&
 current_publication <- clean_publication_data(current_publication,id)
 
 
-##' Gets accurate information about a specific publication
-##'
-##' Gets accurate information from the google scholar page of a publication
-##' Completes and correct partial information gathered from a scholar's personnal page
-##' Each page gives the following information 
-##' title, link to PDF <-- we already have these so we ignore them
-##' author list, publication date, venue, pages <-- we need to add/correct with these
-##'
-##' @param publication a publication as obtained from a scholar's publication list
-##'
-##' @return the updated publication with more accurate information
-clean_publication_data <- function(publication, author_id){
-  #function code starts here
-  
-  page <- "https://scholar.google.com/citations?view_op=view_citation&hl=en"
-  citation_for_view_url <- paste("citation_for_view=",author_id,":",publication$pubid, sep="")
-  author_id_url <- paste("user=",author_id, sep="")
-  page <- paste(page,author_id_url,citation_for_view_url, sep="&")
-  print(page)
-  print("https://scholar.google.com/citations?view_op=view_citation&hl=en&user=ulkW7fgAAAAJ&citation_for_view=ulkW7fgAAAAJ:u-x6o8ySG0sC")
-  pub_page <- get_scholar_resp(page)
-  pub_page <- read_html(pub_page)
-  
-  #We can access all article information from the divs with the class "gsc_oci_value"
-  #Need to remember to add "." to the class or returns null results
-  values <- html_nodes(resp,".gsc_oci_value")
-  publication$author <- html_text(values[1])
-  publication$date <- html_text(values[2])
-  publication$journal <- html_text(values[3])
-  publication$number <- html_text(values[4])
-  return(publication)
-}
 
 
 
---
+++ Publications.R
@@ -0,0 +1,51 @@
+##' Gets accurate information about a researcher's complete list of publications
+##'
+##' 
+##'
+##' This function can be called on a specific publication only
+##' Or can be used to clean a whole list of publications by the function 'clean_all_publications'
+##'
+##' @param author_id the id from the scholar, must be non null
+##' @param publication_list the list of publication from a scholar
+##'
+##' @return the updated publication with more accurate information
+clean_publication_data <- function(publication, author_id){
+
+
+
+##' Gets accurate information about a specific publication
+##'
+##' Gets accurate information from the google scholar page of a publication
+##' Completes and correct partial information gathered from a scholar's personnal page
+##' Each page gives the following information 
+##' title, link to PDF <-- we already have these so we ignore them
+##' author list, publication date, venue, pages <-- we need to add/correct with these
+##'
+##' This function can be called on a specific publication only
+##' Or can be used to clean a whole list of publications by the function 'clean_all_publications'
+##'
+##' @param publication a publication as obtained from a scholar's publication list, must be non null
+##' @param author_id the id of the scholar, must be non null
+##'
+##' @return the updated publication with more accurate information
+clean_publication_data <- function(publication, author_id){
+  #function code starts here
+  
+  page <- "https://scholar.google.com/citations?view_op=view_citation&hl=en"
+  citation_for_view_url <- paste("citation_for_view=",author_id,":",publication$pubid, sep="")
+  author_id_url <- paste("user=",author_id, sep="")
+  page <- paste(page,author_id_url,citation_for_view_url, sep="&")
+  print(page)
+  print("https://scholar.google.com/citations?view_op=view_citation&hl=en&user=ulkW7fgAAAAJ&citation_for_view=ulkW7fgAAAAJ:u-x6o8ySG0sC")
+  pub_page <- get_scholar_resp(page)
+  pub_page <- read_html(pub_page)
+  
+  #We can access all article information from the divs with the class "gsc_oci_value"
+  #Need to remember to add "." to the class or returns null results
+  values <- html_nodes(resp,".gsc_oci_value")
+  publication$author <- html_text(values[1])
+  publication$date <- html_text(values[2])
+  publication$journal <- html_text(values[3])
+  publication$number <- html_text(values[4])
+  return(publication)
+}
